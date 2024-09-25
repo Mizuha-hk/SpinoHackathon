57d5d8f6d82c3594b42532f7e0ca89c9303a20e9
Miz ha <hk.haru0803@gmail.com>
2024-09-25 20:21:30 +0900

    [add] Web App作成

---
+++ SpinoHackathon.WebApp/Extensions/HostingExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace SpinoHackathon.WebApp.Extensions
+{
+    public static class HostingExtensions
+    {
+        public const string OpenIdConnectBackchannel = "OpenIdConnectBackchannel";
+
+        public static void AddAuthenticationServices(this IHostApplicationBuilder builder)
+        {
+            
+        }
+    }
+}
